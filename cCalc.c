8082144dfb857dd0b0ebb659636e647df5ce2fca
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-03-08 10:37:48 -0400

    Update cCalc.c

--- cCalc.c
+++ cCalc.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+int firstNumber, secondNumber;
+char calculation
+
+int main(void)
+{
+  puts("Welcome to cCalc, the best C calculator there!");
+  puts("Please type in the calculation");
+  scanf("%c", &calculation); //ask for calculation
+  return 0;
+}
